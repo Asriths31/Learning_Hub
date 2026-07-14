a6e549b605ae7662862bf52311f98ab78c4e0cee
asriths31 <asrithsai840@gmail.com>
2026-07-15 00:51:14 +0530

    fixed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 
-const baseURL = "https://learning-hub-server.onrender.com"
-// const baseUrl="http://localhost:5000"
+const baseURL = "https://learning-hub-server.onrender.com/api"
+// const baseURL = "http://localhost:5000/api"
 const api = axios.create({
   baseURL,
   withCredentials: true,
